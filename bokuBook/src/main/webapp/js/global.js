89f7b7f598ad0023cb0d163fc0e2ae63218aa21e
Cute Jiu Jiu <17862517955@163.com>
2018-03-28 23:16:51 +0800

    Add events of input field

--- bokuBook/src/main/webapp/js/global.js
+++ bokuBook/src/main/webapp/js/global.js
@@ -3,27 +3,34 @@ var bkyd = {};
 
 //搜索
 bkyd.search = {
-    'form': null,
     'keyword_input': null,
-    'suggestion_url': function (kw) {
-        return 'getSuggestion?keyword=' + kw;
-    },
     'init': function () {
         //输入
         this.keyword_input = $('#keyWords');
         this.keyword_input.on({
             'blur focus click': this.change_input_status,
-            'input propertychange': this.suggestion
+            'input propertychange': this.suggestion,
+            'keypress': function (e) {
+            	if (e.keyCode === 13) {
+            		bkyd.search.submit();
+            	}
+            }
         });
         
-        //空白处点击隐藏suggestion
-        $(document).on('click', function () {
-            $('.suggestion-list').css('visibility', 'hidden');
+        $('.suggestion-list li').on('click', function () {
+                bkyd.search.keyword_input.val($(this).text());
+                bkyd.search.submit();
         });
+        
+        $(document).on('click', function () {
+        	$('.suggestion-list').css('visibility', 'hidden');
+        });
+        
+        $('.search-btn').on('click', this.submit);
+        
     },
     'change_input_status': function (e) {
         if ((e.type == 'focus' || e.type == 'click') && bkyd.search.keyword_input.val() != '') {
-        	console.log($('.suggestion-list li').length);
             if ($('.suggestion-list li').length > 0) {
             	$('.suggestion-list').css('visibility', 'visible');
                 e.stopPropagation();
@@ -35,7 +42,7 @@ bkyd.search = {
     },
     'submit': function () {
         if (bkyd.search.keyword_input.val() != '') {
-            window.location.href = bkyd.search.form.attr('action') + '&keyword=' + bkyd.search.keyword_input.val();
+            window.location.href = 'search?keyword=' + bkyd.search.keyword_input.val();
         }
     },
     'suggestion': function () {
@@ -45,6 +52,25 @@ bkyd.search = {
         	$('.suggestion-list').css('visibility', 'hidden');
             return;
         }
+        
+        // 异步获取suggestion
+        $.ajax({
+			url : 'getSuggestion',
+			async : true,
+			type : 'POST',
+			dataType : 'json',
+			data : {
+				keyWords: kw
+			},
+			success : function(data) {
+				console.log(data);
+				
+				// TODO 操作DOM元素
+			},
+			error : function(err) {
+				console.log(err);
+			}
+		});
         /*$.get(bkyd.search.suggestion_url(encodeURIComponent(kw)), function (ret) {
             if (ret.keywords) {
                 if (bkyd.search.keyword_input.val() == kw) {
