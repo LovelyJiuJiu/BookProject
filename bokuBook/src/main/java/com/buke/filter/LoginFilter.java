93edc4d1477f5d6636379dc0127dab9c6d8c436f
camille <wu.wenqiu1545@qq.com>
2018-04-19 16:00:11 +0800

    无用户登录，强制跳login

---
+++ bokuBook/src/main/java/com/buke/filter/LoginFilter.java
@@ -0,0 +1,41 @@
+package com.buke.filter;  
+  
+import com.boku.pojo.User;
+import javax.servlet.*;  
+import javax.servlet.annotation.WebFilter;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
+@WebFilter(filterName = "LoginFilter")  
+public class LoginFilter implements Filter {  
+    public void destroy() {  
+    }  
+  
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {  
+        HttpServletRequest request = (HttpServletRequest)req;  
+        HttpServletResponse response = (HttpServletResponse)resp;  
+        String uri = request.getRequestURI();
+        String path = uri.substring(uri.lastIndexOf("/"));
+		if ("/login".equals(path) || ("/bookNumber".equals(path)) || ("/login-submit".equals(path))) {
+			chain.doFilter(request, response);
+		} else {
+			HttpSession session = request.getSession();
+			User user = (User) session.getAttribute("currentUser");
+			if (user != null) {
+				chain.doFilter(request, response);
+			} else {
+				response.sendRedirect("login");
+			}
+		}
+    }  
+  
+    public void init(FilterConfig config) throws ServletException {    
+    }  
+  
+}  
